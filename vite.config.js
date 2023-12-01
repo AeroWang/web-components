d6ffda15fe2ae3e74aeca3b2599ea54206f9a15a
Aero <44799877+AeroWang@users.noreply.github.com>
2023-12-01 21:21:07 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-	build: {
-		lib: {
-			entry: {
-				// eslint-disable-next-line no-undef
-				a_sparkles: resolve(__dirname, 'components/Sparkles/index.js'),
-			},
-			name:'aw_components',
-			fileName: (format, name) => {
-				return `components/${name}.${format}.js`;
-			},
-			formats: ['es', 'umd', 'iife']
-		},
-	}
+	// build: {
+	// 	lib: {
+	// 		entry: {
+	// 			// eslint-disable-next-line no-undef
+	// 			a_sparkles: resolve(__dirname, 'components/Sparkles/index.js'),
+	// 		},
+	// 		name:'aw_components',
+	// 		fileName: (format, name) => {
+	// 			return `components/${name}.${format}.js`;
+	// 		},
+	// 		formats: ['es', 'umd', 'iife']
+	// 	},
+	// }
 });
